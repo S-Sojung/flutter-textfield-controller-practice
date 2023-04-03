d99a1eeb3cfe7203369e9cb8414a627e8df20c7b
S-Sojung <80329856+S-Sojung@users.noreply.github.com>
2023-04-03 18:36:34 +0900

    TextField ex

---
+++ android/app/src/main/kotlin/com/example/texteditcontroller_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.texteditcontroller_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Test Application',
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final controller = TextEditingController();
+  final controller2 = TextEditingController();
+  var a;
+  var b;
+  var sum;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Test Application'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              keyboardType: TextInputType.number,
+              controller: controller,
+              // 1
+              maxLines: 1,
+              style: TextStyle(fontSize: 20),
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20)),
+              ),
+              onSubmitted: (value) {
+                a=value;
+              },
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp('[0-9]'))
+              ],
+              controller: controller2,
+              // 1
+              maxLines: 1,
+              style: TextStyle(fontSize: 20),
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20)),
+              ),
+              onSubmitted: (value) {
+                print(value);
+                b=value;
+              },
+            ),
+            TextButton(onPressed: () {
+
+              sum = int.parse(a) + int.parse(b);
+              print(sum);
+            }
+                , child: Text("더하기"))
+          ],
+        ),
+      ),
+    );
+  }
+}
